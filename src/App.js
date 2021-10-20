5cf1b50df476b4d4b50e911d343351de45f635d4
Cathy Diaz <Cathydiaz14@gmail.com>
2021-10-20 16:40:43 -0700

    commit tree a26702a01bdb944ba1034037f0f25a5ec50395b7

--- src/App.js
+++ src/App.js
@@ -7,35 +7,39 @@ class App extends Component {
 
   handleOnClickItems() {
     this.props.dispatch({
-      type: 'GET_COUNT_OF_ITEMS',
+      type: 'INCREASE_COUNT_OF_ITEMS',
     });
   }
 
   handleOnClickUsers() {
     this.props.dispatch({
-      type: 'GET_COUNT_OF_USERS',
+      type: 'INCREASE_COUNT_OF_USERS',
     });
   }
 
   render() {
-    // debugger;
+    debugger;
     return (
       <div className="App">
           <button onClick={() => this.handleOnClickItems()}>
             Click to change items count
             </button>
+            <p>{this.props.items.length}</p>
           <button onClick={() => this.handleOnClickUsers()}>
             Click to change user count
           </button>
-          <p>{this.props.items.length}</p>
+          <p>{this.props.users.length}</p>
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => {
-  debugger;
-  return { items: state.items }
+  return { items: state.items,
+          users: state.users
+          }
 }
 
 export default connect(mapStateToProps)(App);
+
+// can also combile both to equal -> export default connect((state) => ({ items: state.items }))(App);
--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -5,12 +5,12 @@ export default function shoppingListItemReducer(state = {
 
   switch(action.type) {
 
-    case 'GET_COUNT_OF_ITEMS':
+    case 'INCREASE_COUNT_OF_ITEMS':
       return Object.assign({}, state, {
         items: state.items.concat(state.items.length + 1)
       });
 
-    case 'GET_COUNT_OF_USERS':
+    case 'INCREASE_COUNT_OF_USERS':
       return Object.assign({}, state, {
         users: state.users.concat(state.users.length + 1)
       });
